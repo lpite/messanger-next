d9d4416f12b3f5516e22f80acafdfca8d88b44ee
lpite <67955038+lpite@users.noreply.github.com>
2021-09-17 20:42:39 +0300

    спсиок сообщений

---
+++ components/chat-block/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+function ChatBlock() {
+  return (
+    <Link href="/chat/1">
+      <a className="chat-link">
+        <div className="chat-block">
+          <img src="" alt="" className="chat-block-image" />
+          <span className="chat-block-name">name</span>
+          <span className="chat-block-time">time</span>
+          <span className="chat-block-text">message</span>
+        </div>
+      </a>
+    </Link>
+  );
+}
+
+export default ChatBlock;
