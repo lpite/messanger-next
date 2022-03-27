be66a1ac3cf8cc1c8d68be1614b80c617a7d69c1
lpite <farionsasa@gmail.com>
2022-03-27 23:55:21 +0300

    try to remember everything

--- components/new-message/index.tsx
+++ components/new-message/index.tsx
@@ -127,8 +127,8 @@ function NewMessage({ onFocusInput, onBlurInput }) {
       <label className={styles.fileLabel}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
+          width="25"
+          height="25"
           fill="currentColor"
           viewBox="0 0 16 16"
         >
